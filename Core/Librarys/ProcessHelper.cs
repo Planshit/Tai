680e59d78e2422f965664b396c6840e3fa193d61
noberumotto <root@thelittlepandaisbehind.com>
2021-12-26 10:16:02 +0800

    添加ProcessHelper.cs

---
+++ Core/Librarys/ProcessHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Librarys
+{
+    public class ProcessHelper
+    {
+        public static bool Run(string filename, string[] args)
+        {
+            try
+            {
+                string arguments = "";
+                foreach (string arg in args)
+                {
+                    arguments += $"\"{arg}\" ";
+                }
+                arguments = arguments.Trim();
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo(filename, arguments);
+                process.StartInfo = startInfo;
+                process.Start();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.ToString());
+                return false;
+            }
+
+        }
+    }
+}
