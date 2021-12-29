a3d1bb7177e7cc17030d692624c3fc58acbe3d67
noberumotto <root@thelittlepandaisbehind.com>
2021-12-30 03:30:16 +0800

    增加全局键盘钩子

--- Core/Librarys/Win32API.cs
+++ Core/Librarys/Win32API.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -196,6 +197,46 @@ namespace Core.Librarys
             float GetPeakValue();
             // the rest is not defined/needed
         }
+        #endregion
+
+        #region 键盘钩子
+        private const int WH_KEYBOARD_LL = 13;
+        public const int WM_KEYDOWN = 0x0100;
+
+        /// <summary>
+        /// 设置键盘钩子
+        /// </summary>
+        /// <param name="proc"></param>
+        /// <returns></returns>
+        public static IntPtr SetKeyboardHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        public delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+       
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
         #endregion
     }
 }
