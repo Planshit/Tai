1d1e358f54880545e667edf8595b6a8e490ae8b5
noberumotto <root@thelittlepandaisbehind.com>
2022-10-24 15:13:12 +0800

    完善观察服务

--- Core/Servicers/Instances/Observer.cs
+++ Core/Servicers/Instances/Observer.cs
@@ -18,11 +18,8 @@ namespace Core.Servicers.Instances
         private IntPtr hook;
         Win32API.WinEventDelegate winEventDelegate;
 
-
         public event ObserverEventHandler OnAppActive;
 
-
-        //private string activeProcessName, activeProcessFile = null;
         public Observer()
         {
             winEventDelegate = new Win32API.WinEventDelegate(WinEventProc);
@@ -31,68 +28,105 @@ namespace Core.Servicers.Instances
         {
             hook = Win32API.SetWinEventHook(0x0003, 0x0003, IntPtr.Zero,
             winEventDelegate, 0, 0, 0);
+
+            var window = Win32API.GetForegroundWindow();
+            Handle(window);
         }
 
-        private async void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        private void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            Handle(hwnd);
+        }
+
+        private async void Handle(IntPtr hwnd)
         {
             string processName = String.Empty, processFileName = String.Empty, processDescription = String.Empty;
+
             var processResult = await Task.Run(() =>
-              {
-                  try
-                  {
-                      string[] processInfo = GetProcessInfoByWindowHandle(hwnd);
-                      int processID = int.Parse(processInfo[2]);
-                      processName = processInfo[0];
-                      processFileName = processInfo[1];
+            {
+                try
+                {
+                    string[] processInfo = GetProcessInfoByWindowHandle(hwnd);
+                    int processID = int.Parse(processInfo[2]);
+                    processName = processInfo[0];
+                    processFileName = processInfo[1];
 
 
-                      //  handle uwp app
-                      if (processFileName.IndexOf("ApplicationFrameHost.exe") != -1)
-                      {
-                          processFileName = Win32API.UWP_AppName(hwnd, (uint)processID);
-                          if (processFileName == null)
-                          {
-                              Thread.Sleep(1500);
+                    //  handle uwp app
+                    if (processFileName.IndexOf("ApplicationFrameHost.exe") != -1)
+                    {
+                        processFileName = Win32API.UWP_AppName(hwnd, (uint)processID);
+                        if (processFileName == null)
+                        {
+                            Thread.Sleep(1500);
 
-                              processFileName = Win32API.UWP_AppName(hwnd, (uint)processID);
-                              processName = GetUWPAPPName(processFileName);
+                            processFileName = Win32API.UWP_AppName(hwnd, (uint)processID);
+                            processName = GetUWPAPPName(processFileName);
 
-                              IntPtr foregeroundHwnd = Win32API.GetForegroundWindow();
+                            IntPtr foregeroundHwnd = Win32API.GetForegroundWindow();
 
-                              if (foregeroundHwnd != hwnd)
-                              {
-                                  //    获取后焦点变了
-                                  processInfo = GetProcessInfoByWindowHandle(foregeroundHwnd);
-                                  processID = int.Parse(processInfo[2]);
-                                  processName = processInfo[0];
-                                  processFileName = processInfo[1];
-                              }
+                            if (foregeroundHwnd != hwnd)
+                            {
+                                //    获取后焦点变了
+                                processInfo = GetProcessInfoByWindowHandle(foregeroundHwnd);
+                                processID = int.Parse(processInfo[2]);
+                                processName = processInfo[0];
+                                processFileName = processInfo[1];
+                            }
 
-                          }
-                          else
-                          {
-                              processName = GetUWPAPPName(processFileName);
-                          }
+                        }
+                        else
+                        {
+                            processName = GetUWPAPPName(processFileName);
+                        }
 
-                      }
-                      Debug.WriteLine("file name: ---------> " + " | " + hwnd + " | " + processID + processFileName);
-                      Debug.WriteLine("app name: ---------> " + processName);
-                      FileVersionInfo info = FileVersionInfo.GetVersionInfo(processFileName);
-                      processDescription = info.FileDescription;
+                    }
+                    FileVersionInfo info = FileVersionInfo.GetVersionInfo(processFileName);
+                    processDescription = info.FileDescription;
 
 
-                      //Debug.WriteLine("file name: ---------> " + processFileName);
-                      //Debug.WriteLine("app name: ---------> " + processName);
-                      //Debug.WriteLine("processDescription: ---------> " + processDescription);
-                  }
-                  catch (Exception e)
-                  {
-                      Logger.Warn(e.Message + " | Process Name:" + processName + " | Process File:" + processFileName + " | Process Description:" + processDescription);
-                  }
-                  return processName;
-              });
+                    int titleCapacity = Win32API.GetWindowTextLength(hwnd) * 2;
+                    StringBuilder stringBuilder = new StringBuilder(titleCapacity);
+                    Win32API.GetWindowText(hwnd, stringBuilder, stringBuilder.Capacity);
+                    if (IsSystemProcess(processName, stringBuilder.ToString()))
+                    {
+                        //  过滤掉一些系统进程
+                        processName = string.Empty;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Warn(e.Message + " | Process Name:" + processName + " | Process File:" + processFileName + " | Process Description:" + processDescription);
+                }
+                return processName;
+            });
 
             EventInvoke(processName, processDescription, processFileName);
+        }
+
+        /// <summary>
+        /// 指示指定进程是否属于系统进程
+        /// </summary>
+        /// <param name="processName">进程名称</param>
+        /// <param name="title">窗口标题</param>
+        /// <returns></returns>
+        private bool IsSystemProcess(string processName, string title)
+        {
+            processName = processName.ToLower();
+            int length = title.Length;
+            string[] titleArr = { "UnlockingWindow", "Program Manager" };
+
+            if (processName == "explorer")
+            {
+                if (length == 0)
+                {
+                    //  可能是任务栏
+                    return true;
+                }
+                return titleArr.Contains(title);
+
+            }
+            return false;
         }
 
         private string GetUWPAPPName(string processFileName)
@@ -137,19 +171,6 @@ namespace Core.Servicers.Instances
         }
         private void EventInvoke(string processName, string description, string filename)
         {
-            //  防止重复和错误响应
-            //if (string.IsNullOrEmpty(processName) || string.IsNullOrEmpty(filename) || !File.Exists(filename))
-            //{
-            //    return;
-            //}
-            //if (filename == activeProcessFile && processName == activeProcessName)
-            //{
-            //    return;
-            //}
-
-            //activeProcessName = processName;
-            //activeProcessFile = filename;
-
             OnAppActive?.Invoke(processName, description, filename);
         }
 
