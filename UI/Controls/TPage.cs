fc4e0596e3918d93d238968a658f5d223de9c465
noberumotto <root@thelittlepandaisbehind.com>
2022-11-27 04:28:22 +0800

    增加 TPage 控件

---
+++ UI/Controls/TPage.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace UI.Controls
+{
+    public class TPage : Page
+    {
+        /// <summary>
+        /// 是否填充页面而不使用自适应滚动
+        /// </summary>
+        public bool IsFillPage
+        {
+            get { return (bool)GetValue(IsFillPageDependencyProperty); }
+            set { SetValue(IsFillPageDependencyProperty, value); }
+        }
+        public static DependencyProperty IsFillPageDependencyProperty = DependencyProperty.Register("IsFillPage", typeof(bool), typeof(TPage));
+
+        private PageContainer pageContainer;
+        public TPage()
+        {
+            DefaultStyleKey = typeof(TPage);
+            Loaded += TPage_Loaded;
+        }
+
+        private void TPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdatePageSize();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            HandleEvent();
+        }
+
+        private void HandleEvent()
+        {
+            if (pageContainer != null)
+            {
+                pageContainer.SizeChanged -= Pc_SizeChanged;
+            }
+
+            if (IsFillPage)
+            {
+                //  查找父容器
+                var parent = VisualTreeHelper.GetParent(this);
+                if (parent != null)
+                {
+                    while (!(parent is PageContainer))
+                    {
+                        parent = VisualTreeHelper.GetParent(parent);
+                    }
+                }
+
+                pageContainer = parent as PageContainer;
+                if (pageContainer != null)
+                {
+                    pageContainer.SizeChanged += Pc_SizeChanged;
+                }
+            }
+        }
+
+        private void Pc_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdatePageSize();
+        }
+
+        private void UpdatePageSize()
+        {
+            Width = pageContainer.ActualWidth;
+            Height = pageContainer.ActualHeight;
+
+            Debug.WriteLine("UpdatePageSize");
+        }
+    }
+}
